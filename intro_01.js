2512d53d125c2364d0e249c36e30f883e3a6b885
kfire <00kfire00@gmail.com>
2014-09-27 15:07:31 +0900

    추가

---
+++ intro_01.js
@@ -0,0 +1,15 @@
+    $(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528
+      });
+    });
+
+     $(function() {
+      $('#slides_01').slidesjs({
+        width: 940,
+        height: 528
+      });
+    });
+
+//opacity: 0.0;
